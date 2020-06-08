9b35a4a1657a82cc97287105f0fcbf960352a9b0
Daniel Jenkala <djenkala@yahoo.com>
2020-06-08 17:49:45 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -16,3 +16,8 @@ end
     expect(page).to have_css("h1", text: "My Post")
   end
 end
+
+it 'shows the description on the show page in a p tag' do
+  visit "/posts/#{@post.id}"
+  expect(page).to have_css("p", text: "My post desc")
+end
